f3f1e82301055c2c15dd8b2b16b65b895da2280a
unknown <RR118@ccs.local>
2026-05-21 15:49:01 +0800

    fix: load swagger.yaml relative to __dirname to prevent startup crash

--- _helpers/swagger.ts
+++ _helpers/swagger.ts
@@ -1,9 +1,10 @@
 import express from 'express';
 import swaggerUi from 'swagger-ui-express';
 import YAML from 'yamljs';
+import path from 'path';
 
 const router = express.Router();
-const swaggerDocument = YAML.load('./swagger.yaml');
+const swaggerDocument = YAML.load(path.join(__dirname, '../swagger.yaml'));
 
 router.use('/', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
