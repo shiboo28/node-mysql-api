07626aad2be9f779d81ddcffcd9c0481c3422358
shiboo28 <jivolisao12345@gmail.com>
2026-05-26 19:19:25 +0800

    fix: gmail

--- _helpers/send-email.ts
+++ _helpers/send-email.ts
@@ -6,9 +6,6 @@ async function getTransporter() {
             host: process.env.SMTP_HOST,
             port: parseInt(process.env.SMTP_PORT || '587'),
             secure: false,
-            tls: {
-                rejectUnauthorized: false
-            },
             auth: {
                 user: process.env.SMTP_USER,
                 pass: process.env.SMTP_PASS
